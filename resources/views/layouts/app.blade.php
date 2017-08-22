0c4f5f35f8a6838412b902c9433fd0b1e2edb5c9
ThanuSin <khunrebirth@gmail.com>
2017-08-22 12:05:22 +0700

    Add Route to Home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
       <nav class="navbar ">
         <div class="navbar-brand">
-          <a class="navbar-item" href="http://bulma.io">
+          <a class="navbar-item" href="{{ route('markets.index') }}">
             <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
           </a>
 
